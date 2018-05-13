aeb614f17e4e311f48c2d3d6f0c6b72ccbe1c8ee
hugo <hugo@nuaa.edu.cn>
2018-05-13 12:37:48 +0800

    update

--- mycat/mycat.c
+++ mycat/mycat.c
@@ -3,28 +3,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 // int open(const char *pathname, int flags);
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     char *path = argv[1];
     int file;
     file = open(path, O_RDONLY);
-    if (file == -1) {
+    if (file == -1)
+    {
         printf("unable to open %s", argv[1]);
     }
     char *buf[100000];
     int length;
     length = read(file, buf, 100000);
-    if (length != -1){
+    if (length != -1)
+    {
         printf("%s", buf);
-       // printf("%d", length);
+        // printf("%d", length);
     }
-    else {
+    else
+    {
         printf("unable to open %s", argv[1]);
     }
 
     close(file);
     printf("\n");
     return 0;
-
 }
--- mysys/mysys.c
+++ mysys/mysys.c
@@ -1,22 +1,91 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
-void mysys(char str[])
+char **split(char str[])
 {
+    // printf("%s\n", str);
+    // char str[] = "echo qwe sdfsa sdaf";
+    char **res = NULL;
+    char *p = strtok(str, " ");
+    int n_spaces = 0, i;
 
-    int error;
-    pid_t pid;
-    char 
-    pch = strtok(str," ");
-    pid = fork();
-    if (pid == 0) {
-       error =  execlp("/bin/echo", "echo", "a", "b", "c", NULL); 
-       if (error < 0)
-           perror("execl");
+    /* split string and append tokens to 'res' */
+
+    while (p)
+    {
+        res = realloc(res, sizeof(char *) * ++n_spaces);
+
+        if (res == NULL)
+            exit(-1); /* memory allocation failed */
+
+        res[n_spaces - 1] = p;
+
+        p = strtok(NULL, " ");
     }
+
+    /* realloc one extra element for the last NULL */
+
+    res = realloc(res, sizeof(char *) * (n_spaces + 1));
+    res[n_spaces] = 0;
+
+    // for (i = 0; i < (n_spaces + 1); ++i)
+    //     printf("res[%d] = %s\n", i, res[i]);
+
+    /* free the memory allocated */
+
+    free(res);
+    return res;
 }
 
+int main(int argc, char *argv[])
+{   
+    // int error;
+    pid_t pid;
+    // char pch = strtok(str, " ");
+    pid = fork();
+    if (pid == 0)
+    {   
+        char str[] = "echo ad b c d";
+        char **str_temp = split(str);
+        int error;
+        error = execl("/bin/echo", str_temp[0], str_temp[1], str_temp[2], NULL);
+        if (error < 0)
+            perror("execl");
+    }
+    else
+    {
+        char str[] = "ls /";
+        char **str_temp = split(str);
+        int error;
+        error = execl("/bin/ls", str_temp[0], str_temp[1], NULL);
+        if (error < 0)
+            perror("execl");
+    }
 
 
+    printf("--------------------------------------------------\n");
+    char str[] = "echo HELLO WORLD nmm";
+    char **str_array = split(str);
+    return 0;
+}
 
+// void mysys(char str[])
+// {
+
+//     int error;
+//     pid_t pid;
+//     char pch = strtok(str, " ");
+//     pid = fork();
+//     if (pid == 0)
+//     {
+//         error = execlp("/bin/echo", "echo", "a", "b", "c", NULL);
+//         if (error < 0)
+//             perror("execl");
+//     }
+//     else
+//     {
+
+//     }
+// }
